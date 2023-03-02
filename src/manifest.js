0976897eec3f1e30965226d99557d69a21dd3424
chergav <gav3253975@gmail.com>
2023-03-02 14:27:13 +0300

    fix

--- src/manifest.js
+++ src/manifest.js
@@ -41,7 +41,7 @@ export default defineManifest(() => ({
 		'activeTab',
 		'storage'
 	],
-	host_permissions: ['https://translate.googleapis.com/*'],
+	host_permissions: ['<all_urls>', 'https://translate.googleapis.com/*'],
 	web_accessible_resources: [
 		{
 			matches: ['<all_urls>'],
